d62f0a1649380c81fcd3ee0c47632ab9849242e1
Nursultan <nursmaster@gmail.com>
2019-02-14 18:34:26 +0200

    HTML, CSS, JS Ex5 is done

--- myFunction.js
+++ myFunction.js
@@ -1,14 +1,10 @@
-function loadXMLDoc(dname)
-{
-if (window.XMLHttpRequest)
-{
-xhttp=new XMLHttpRequest();
-}
-else
-{
-xhttp=new ActiveXObject("Microsoft.XMLHTTP");
-}
-xhttp.open("GET",dname,false);
-xhttp.send();
-return xhttp.responseXML;
-}
+function loadXMLDoc(page) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+        document.getElementById("content").innerHTML = this.responseText;
+        }
+    };
+    xhttp.open("GET", page, true);
+    xhttp.send();
+    }
